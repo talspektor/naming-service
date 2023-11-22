4f75b8f41c324d54ec9757d07a21ab7bb9a01675
tspektor <tal_spektor@intuit.com>
2023-11-22 09:04:04 +0200

    Initial commit

---
+++ src/test/java/com/tspektor/microservices/nameingservice/NameingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tspektor.microservices.nameingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NameingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
